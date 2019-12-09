b8202a15eb72f90b2b352dabed520d40cd1c5e92
lucas <lucasleproust@gmail.com>
2019-12-09 09:47:16 +0100

    create menu burger and add menu to layout

--- src/components/Layouts/Burger.jsx
+++ src/components/Layouts/Burger.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { Icon, Button, Menu, Segment, Sidebar } from 'semantic-ui-react'
 
-function Burger() {
+export default function Burger() {
+  const [visible, setVisible] = useState('visible' )
+
   return (
-    <header>
-        <h1>C'est la navbar lol</h1>
-    </header>
-  );
-}
+    <>
+      <Sidebar
+        as={Menu}
+        animation='overlay'
+        icon='labeled'
+        inverted
+        onHide={() => setVisible(false)}
+        vertical
+        visible={visible}
+        width='thin'
+      >
+        <Menu.Item as='a'>
+          Home
+        </Menu.Item>
+        <Menu.Item as='a'>
+          Games
+        </Menu.Item>
+        <Menu.Item as='a'>
+          Channels
+        </Menu.Item>
+      </Sidebar>
 
-export default Burger;
+      <Sidebar.Pusher>
+        <Segment basic>
+          <Button onClick={()=>setVisible(!visible)} >
+            <Icon className='bars'></Icon>
+          </Button>
+        </Segment>
+      </Sidebar.Pusher>
+    </>
+  )
+}
--- src/components/Layouts/MainLayout.jsx
+++ src/components/Layouts/MainLayout.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Segment, Sidebar } from 'semantic-ui-react'
 
 import Burger from './Burger';
 
 function MainLayout({children}) {
   return (
     <>
+    <Sidebar.Pushable as={Segment}>
       <Burger />
       <div>{children}</div>
+    </Sidebar.Pushable>
     </>
   );
 }
