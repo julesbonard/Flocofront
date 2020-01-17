7f6f4f077ae5a1dc1fde07f0b435d8f6dc6cf9f1
lucas <lucasleproust@gmail.com>
2020-01-17 12:13:01 +0100

    add button

--- src/components/Main/Map/Modal.jsx
+++ src/components/Main/Map/Modal.jsx
@@ -1,49 +1,35 @@
 import React, { useState } from 'react'
-import { Button, Header, Image, Modal,  } from 'semantic-ui-react'
+import { Button, Header, Image, Modal, } from 'semantic-ui-react'
 import { axios } from 'axios';
 
+function ModalMarker({ open, setPlantUuid, addMarker, closeModal }) {
+  const createPlant = () => {
+    axios.post("/plant")
+      .then(res => {
+        setPlantUuid(res.data.uuid)
+        return addMarker()
+      })
+      .catch(err => {
+        console.log(err)
+      })
+      .finally(() => closeModal())
+  }
 
-
-function ModalMarker ({open, setPlantUuid, addMarker, closeModal}) {
-
-    const createPlant = () => {
-        axios.post("/plant")
-        .then(res => {
-            setPlantUuid(res.data.uuid)
-            return addMarker()
-        })
-        .catch(err => {
-            console.log(err)
-        })
-        .finally(() => closeModal())
-    }
-
-    return (
-  <Modal open={open}>
-    <Modal.Header>Récapitulatif : </Modal.Header>
-    <Modal.Content image>
-      <Image wrapped size='medium' src='https://react.semantic-ui.com/images/avatar/large/rachel.png' />
-      <Modal.Description>
-        <Header>Nom de la plante</Header>
-        <p>
-          Type de pot:</p>
-          <p>interieur ou exterieur</p> 
-         <p> graines (menu deroualnt )
-        </p>
-      </Modal.Description>
-    </Modal.Content>
-  </Modal>
-    )};
-
-
+  return (
+    <Modal open={open}>
+      <Modal.Header>Récapitulatif : </Modal.Header>
+      <Modal.Content image>
+        <Image wrapped size='medium' src='https://react.semantic-ui.com/images/avatar/large/rachel.png' />
+        <Modal.Description>
+          <Header>Nom de la plante</Header>
+          <p>Type de pot:</p>
+          <p>interieur ou exterieur</p>
+          <p> graines (menu deroualnt )</p>
+        </Modal.Description>
+      </Modal.Content>
+      <Button onClick={() => { createPlant() }}>Planter une plante</Button>
+    </Modal>
+  )
+};
 
 export default ModalMarker;
-
-
-
-
-
-
-
-
-
