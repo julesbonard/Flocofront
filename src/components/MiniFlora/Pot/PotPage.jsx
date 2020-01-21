80f29ff05079e9ca85ec87619a99e4c1a846ef7e
DOMINIC BRICE <dominicbrice@MBP-de-DOMINIC.lan>
2020-01-21 16:49:58 +0100

    css en dur de la page pot

--- src/components/MiniFlora/Pot/PotPage.jsx
+++ src/components/MiniFlora/Pot/PotPage.jsx
@@ -1,33 +1,36 @@
 import React from 'react';
 import styles from './PotPage.module.css';
-import { Image, Segment } from "semantic-ui-react"
+import { Image, Grid, } from "semantic-ui-react"
 import pot from '../../../assets/jardiniere.png'
 import basImage from '../../../assets/footer.png'
 
 
 
-const { longueur, largeur, profondeur, potFleur, image, centrage, inputlong, inputlarg, inputprof, long, larg, prof  } = styles;
+const { longueur, largeur, profondeur, potFleur, image, long, larg, prof, centragePage, imageBas  } = styles;
 
 function PotPage() {
   return (
     <>
-      <Image size="medium" className={potFleur} src={pot}/>
-      <div className={centrage}>
-        <div className={inputlong}>
-      <p className={longueur}>LONGUEUR </p>
-      <input className={long} type="text" placeholder="Longueur"/> cm
-        </div>
-        <div className={inputlarg}>
-      <p className={largeur}>LARGEUR</p>
-      <input className={larg} type="text" placeholder="Largeur" /> cm
-        </div>
-        <div className={inputprof}>
-      <p className={profondeur}>PROFONDEUR</p>
-      <input className={prof} type="text" placeholder="Profondeur"/> cm
-        </div>
-      </div>
-      <Image className={image} src={basImage}/>
-      
+     <Grid className={centragePage}>
+       <Grid.Row >
+        <Image size="medium" className={potFleur} src={pot}/>
+        </Grid.Row>
+            <Grid.Row >
+              <p className={longueur}>LONGUEUR </p>
+               <input className={long} type="text" placeholder="Longueur"/>
+        </Grid.Row>
+          <Grid.Row >
+            <p className={largeur}>LARGEUR</p>
+              <input className={larg} type="text" placeholder="Largeur" /> 
+          </Grid.Row>
+            <Grid.Row >
+              <p className={profondeur}>PROFONDEUR</p>
+                <input className={prof} type="text" placeholder="Profondeur"/>
+            </Grid.Row>
+              <Grid.Row className={imageBas}>
+            <Image size="large" className={image} src={basImage}/>
+              </Grid.Row>
+        </Grid>
     </>
   )
 };
