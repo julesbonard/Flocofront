7e925b0d2d9f92ee7bffd574a8e5483b64398b9b
lucas <lucasleproust@gmail.com>
2020-01-24 11:40:20 +0100

    change miniflora

--- src/components/MiniFlora/Partner/PartnerPage.jsx
+++ src/components/MiniFlora/Partner/PartnerPage.jsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import { Image, Grid } from 'semantic-ui-react';
-import styles from './PartnerPage.module.css';
+import React from "react";
+import { Image, Grid } from "semantic-ui-react";
+import styles from "./PartnerPage.module.css";
 
-const { card, title, partnerimage, laSerre, partnername, partnercoordonate, navbar } = styles;
+import laSerre from "../../../assets/LaSerre.png";
+import jardinissler from "../../../assets/JardinIssler.png";
+import borderbottom from "../../../assets/border-bas.png";
+
+const {
+  fullpage,
+  card,
+  title,
+  partnerimage,
+  partnername,
+  partnercoordonate,
+  endpage
+} = styles;
 
 function PartnerPage() {
   return (
     <>
-      <div className={card}>
-        <Grid>
-          <Grid.Column textAlign='center'>
-            <p className={title}>Mini-Flore</p>
-            <Image className={partnerimage} src={laSerre} centered />
-            <h1 className={partnername}>La Serre</h1>
-            <p className={partnercoordonate}>Coordonnées</p>
-          </Grid.Column>
+      <p className={title}>Mini-Flore</p>
+      <Grid className={fullpage}>
+        <Grid className={card}>
+          <Grid.Row textAlign="center">
+            <Image className={partnerimage} src={jardinissler} centered />
+            <article>
+              <h1 className={partnername}>Jardins Issler</h1>
+              <p className={partnercoordonate}>
+                Rue du commerce - 67550 Vendenheim
+              </p>
+              <p>Tel : 03 88 33 97 33 - Fax : 03 88 33 97 94</p>
+              <p>www.jardins-issler.fr</p>
+            </article>
+          </Grid.Row>
         </Grid>
-      </div>
-      <p className={navbar}>ITINERAIRE</p>
-
+        <Grid className={card}>
+          <Grid.Row textAlign="center">
+            <Image className={partnerimage} src={laSerre} centered />
+            <article>
+              <h1 className={partnername}>La Serre</h1>
+              <p className={partnercoordonate}>
+                11 rue de Sélestat - 67300 Schiltigheim
+              </p>
+              <p>Tel : 03 88 33 06 85 - Fax : 03 88 19 15 59</p>
+              <p>info@la-serre.net - www.la-serre.net</p>
+            </article>
+          </Grid.Row>
+        </Grid>
+      </Grid>
+      <Grid className={endpage}>
+        <Image size="large" src={borderbottom} />
+      </Grid>
     </>
-  )
-};
+  );
+}
 
 export default PartnerPage;
--- src/components/MiniFlora/Progress/ProgressPage.jsx
+++ src/components/MiniFlora/Progress/ProgressPage.jsx
@@ -1,51 +1,74 @@
-import React from 'react';
-import { Image, Grid } from 'semantic-ui-react';
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Image, Grid } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-import styles from './ProgressPage.module.scss';
+import styles from "./ProgressPage.module.scss";
 
-import iconeplus from '../../../assets/croix.png'
+import borderb from "../../../assets/border-bas.png";
+import iconeplus from "../../../assets/croix.png";
 
-const { levelLock, levelUnlock, cercleIcon } = styles;
+const {
+  page,
+  box,
+  levelLock,
+  levelUnlock,
+  cercleIcon,
+  partner,
+  endpage
+} = styles;
 function ProgressPage({ level }) {
-  let one = false
-  let two = false
-  let three = false
+  let one = false;
+  let two = false;
+  let three = false;
 
   if (level >= 1) {
-    one = true
+    one = true;
   }
   if (level >= 2) {
-    two = true
+    two = true;
   }
   if (level >= 3) {
-    three = true
+    three = true;
   }
   return (
-    <Grid>
-      <Grid.Row >
-        <Link to="/library" className={three ? null : levelLock} >
-          <p className={three ? levelUnlock : null}>Quelle maison habiterait ta plante?</p>
+    <Grid className={page}>
+      <Grid.Row>
+        <Link to="/Boussole" className={three ? null : levelLock}>
+          <p className={three ? levelUnlock : null}>
+            Votre plante rique t'elle d'être bronzée?
+          </p>
         </Link>
       </Grid.Row>
       <Grid.Row>
-        <Link to="/compass" className={two ? null : levelLock}>
-          <p className={two ? levelUnlock : null}>Quelle maison habiterait ta plante?</p>
+        <Link to="./Pot" className={two ? null : levelLock}>
+          <p className={two ? levelUnlock : null}>
+            Quelle maison habiterait ta plante?
+          </p>
         </Link>
       </Grid.Row>
       <Image className={cercleIcon} src={iconeplus} />
-      <Grid.Row >
-        <Link to="/access" className={one ? null : levelLock}>
-          <p className={one ? levelUnlock : null}>La restauration de la Flore locale compte sur toi...</p>
+      <Grid.Row className={box}>
+        <Link to="./Bibliotheque" className={one ? null : levelLock}>
+          <p className={one ? levelUnlock : null}>
+            La restauration de la Flore locale compte sur toi...
+          </p>
         </Link>
       </Grid.Row>
+      <Grid.Row>
+        <Link to="./Partner" className={partner}>
+          Découvrez les propositions de nos partenaires
+        </Link>
+      </Grid.Row>
+      <Grid.Row>
+        <Image className={endpage} size="large" src={borderb} />
+      </Grid.Row>
     </Grid>
-  )
-};
+  );
+}
 
 const mapStateToProps = state => {
   return {
-    level: state.levelReducer.level.level,
+    level: state.levelReducer.level.level
   };
 };
 
